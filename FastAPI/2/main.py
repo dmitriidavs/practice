a09cc8141a8bd9885154ddebab26f06b0b70f477
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-11 14:21:40 +0400

    Update main.py

--- FastAPI/2/main.py
+++ FastAPI/2/main.py
@@ -1,7 +1,7 @@
 from enum import Enum
-from typing import Optional, Union
+from typing import Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -65,3 +65,59 @@ def query_item_by_params(name: Optional[str] = None,
         'selection': selection
     }
 
+
+@app.post('/')
+def add_item(item: Item) -> dict[str, Item]:
+
+    if item.id in items:
+        HTTPException(
+            status_code=400, detail=f'Item with {item.id=} already exists.'
+        )
+
+    items[item.id] = item
+    return {'added': item}
+
+
+@app.put(
+    '/update/{item_id}',
+    responses={
+        404: {'description': 'Item not found'},
+        400: {'description': 'No args specified'},
+    },
+)
+def update(item_id: int = Path(ge=0),   # item_id >= 0
+           name: Optional[str] = Query(default=None, min_length=1, max_length=8),   # 1 <= len(name) <= 8
+           price: Optional[float] = Query(default=None, gt=0.0),    # price > 0.0
+           count: Optional[int] = Query(default=None, ge=0)) -> dict[str, Item]:
+
+    if item_id not in items:
+        HTTPException(
+            status_code=400, detail=f'Item with {item_id=} does not exist.'
+        )
+    if all(info is None for info in (name, price, count)):
+        raise HTTPException(
+            status_code=400, detail='No parameters provided for update.'
+        )
+
+    item = items[item_id]
+    if name is not None:
+        item.name = name
+    if price is not None:
+        item.price = price
+    if count is not None:
+        item.count = count
+
+    return {'updated': item}
+
+
+@app.delete('/delete/{item_id}')
+def delete_item(item_id: int) -> dict[str, Item]:
+
+    if item_id not in items:
+        raise HTTPException(
+            status_code=400, detail=f'Item with {item_id=} does not exist.'
+        )
+
+    item = items.pop(item_id)
+
+    return {'deleted': item}
