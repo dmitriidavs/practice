d9a74d836ac9d9caba8aecd9e472a6916fc97b07
Dmitry.Davletshin <dmitriidavs@gmail.com>
2023-04-17 20:56:49 +0400

    Create creds.py

---
+++ AWS/lambda/creds.py
@@ -0,0 +1,11 @@
+AIRFLOW_API_URL = 'localhost:8080'
+
+AIRFLOW_API_AUTH = ('airflow', 'airflow')
+
+AIRFLOW_API_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+DAG_ID_TO_CLONE = 'test_base_dag'
+CLONED_DAG_ID = 'test_base_8431_dag'
